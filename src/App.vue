228df5152a63154e66e6fb04f1fcb44ae6b2eaf6
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-09-05 19:54:19 +1000

    switched from options API to composition API, completed Transactions component and moved to global state

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,26 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList v-bind:transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup>
   import Header from './components/Header.vue'
   import Balance from './components/Balance.vue'
   import IncomeExpenses from './components/IncomeExpenses.vue'
+  // passing down transactions array to TransactionList
   import TransactionList from './components/TransactionList.vue'
   import AddTransaction from './components/AddTransaction.vue'
-  export default {
-    components: {
-      Header,
-      Balance,
-      IncomeExpenses,
-      TransactionList,
-      AddTransaction
-    }
-  }
+
+  // used to turn something reactive
+  import { ref } from 'vue'
+
+  const transactions = ref([
+    {id: 1, text: 'Flowers', amount: -67.91},
+    {id: 2, text: 'Salary', amount: 705.44},
+    {id: 3, text: 'Book', amount: -24.99},
+    {id: 4, text: 'Camera', amount: -2559},
+  ])  
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,26 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            Paycheck <span>$800</span><button class="delete-btn">x</button>
+        <li 
+            v-for="transaction in transactions" 
+            v-bind:key="transaction.id"
+            v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{ transaction.text }} <span>${{transaction.amount}}</span>
+            <button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
+
+<script setup>
+    // defineProps lets you access props from parent component
+    import { defineProps } from 'vue'
+
+    const props = defineProps({
+        // single prop
+        transactions: {
+            type: Array,
+            Required: true
+        }
+    })
+</script>
